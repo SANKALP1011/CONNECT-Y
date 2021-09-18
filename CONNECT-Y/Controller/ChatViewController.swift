f60174a5f186397e256373d4369e80582d872c19
SANKALP <tigerishan426@gmail.com>
2021-09-18 21:49:53 +0530

    User Validation.

--- CONNECT-Y/Controller/ChatViewController.swift
+++ CONNECT-Y/Controller/ChatViewController.swift
@@ -14,7 +14,16 @@ class ChatViewController: UIViewController
         
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-      
+      userValidate()
+    }
+    
+    private func userValidate(){
+        if FirebaseAuth.Auth.auth().currentUser == nil{
+            performSegue(withIdentifier: "goToLog", sender: self)
+        }
+        else{
+            print("user Logged In")
+        }
     }
     
  
